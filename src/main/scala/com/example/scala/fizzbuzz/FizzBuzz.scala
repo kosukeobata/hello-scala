60fe4ea2b7cbb4a1b6b360018955829729e012ec
kosukeobata <kosuke.obata@bizreach.co.jp>
2015-06-24 21:48:40 +0900

    fizzbuzz更新

--- src/main/scala/com/example/scala/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/com/example/scala/fizzbuzz/FizzBuzz.scala
@@ -2,16 +2,30 @@ package com.example.scala.fizzbuzz
 
 object FizzBuzz {
 
-  def main(args: Array[String]): Unit = {
-    // ヒント
-    (1 to 100).foreach { x =>
-      // 剰余算
-      if(x % 2 == 0) {
-        "偶数です"
-      } else {
-        println(x)
-      }
-    }
+  def toStr(x: Int): String = x match {
+    case x if x % 15 == 0 => "ウェイウェーイ"
+    case x if x % 3 == 0  => "ウェイ"
+    case x if x % 5 == 0  => "ウェーイ"
+    case _                => x.toString
   }
 
+  def fizzbuzz1() = {
+    (1 to 100).map(toStr).foreach(println)
+  }
+
+  def fizzbuzz2() = {
+    (1 to 100).filter(x => x % 2 != 0).map(toStr).foreach(println)
+  }
+
+  def fizzbuzz3() = {
+    (1 to 100).filter(x => x % 2 != 0).map(toStr).foreach(x => print(x + ", "))
+  }
+
+  def fizzbuzz4() = {
+    println((1 to 100).filter(x => (x % 2 != 0) && (x % 3 != 0) && (x % 5 != 0)).sum)
+  }
+
+  def main(args: Array[String]): Unit = {
+    fizzbuzz3()
+  }
 }
