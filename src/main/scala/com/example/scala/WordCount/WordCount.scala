01a60763ccd6fe2a2123844a4d889b8b7cafbd8d
kosukeobata <kosuke.obata@bizreach.co.jp>
2015-06-26 17:09:11 +0900

    final commit

--- src/main/scala/com/example/scala/WordCount/WordCount.scala
+++ src/main/scala/com/example/scala/WordCount/WordCount.scala
@@ -2,28 +2,37 @@ package com.example.scala.WordCount
 
 object WordCount {
 
-//  def countFruits(lines: List[String]): Map[String, Int] = {
-//    Map("", 5)
-//  }
+  def countFruits(lines: List[String]): Map[String, Int] = {
+    val g = lines.flatMap(x => x.split(" "))
+    val a = g.foldLeft(Map.empty[String, Int]){ (key, value) =>
+      val newvalue = key.getOrElse(value, 0)
+      key + (value -> (1 + newvalue))
+    }
+    return a
+  }
+
+
+  def func(x: Int): Int = x + 1
+
+  /* 渦巻きマークは再帰 */
+  def f(x: Int): Int = f(x - 1) + x
+
+  /* 矢印の円マークは末尾再帰 */
+  def f2(x: Int, acc: Int = 0): Int = {
+    f2(x -1 , acc + x)
+  }
+
+  def factorial(n: Int): Long = {
+    @scala.annotation.tailrec
+    def fac0(n: Int, acc: Int = 1): Long = {
+    if (n == 0) acc
+      else fac0(n - 1, n * acc)
+    }
+    fac0(n)
+  }
 
   def main(args: Array[String]) {
     val fruits = List("apple banana", "orange apple mango", "kiwi papaya orange","mango orange muscat apple")
-    val g = fruits.map(x => x.split(" ")).flatten
-    println(g.distinct)
-    println(g)
-    val a = g.foldLeft(Map.empty[String, Int]){ (fruitmap, fruit) =>
-      val value = fruitmap.get(fruit)
-      fruitmap + (fruit -> {
-        1 + value.getOrElse(0)
-      })
-    }
-    println(a)
-//    a.map(x => println(x))
-//    for ((key, value) <- a) if (key == Nil) 1 else 0
-//        a.foreach { kv =>
-//          val (key, value) = kv
-//          println(s"key:$key, value:$value")
-//          println("key: " + key + ", value: " + value)
-//        }
+    println(countFruits(fruits))
   }
 }
--- src/main/scala/com/example/scala/sample/Sample.scala
+++ src/main/scala/com/example/scala/sample/Sample.scala
@@ -3,14 +3,9 @@ package com.example.scala.sample
 
 class Sample {
 
-  def plus(x: Int, y: Int): Int = {
-    return x.+(y);
-  }
+  def plus(x: Int, y: Int) = x.+(y)
 
-
-  def minus(x: Int, y: Int): Int = {
-    return x.-(y);
-  }
+  def minus(x: Int, y: Int) = x.-(y)
 
 
   def before(): Unit = {
@@ -43,6 +38,34 @@ class Sample {
         return (a._1, a._2.+(1));
       }
     })
+  }
+
+
+/* できる限りbeforeを省略したメソッド */
+  def after(): Unit = {
+    val list = List(1, 2, 3)
+
+    val total = list.foldLeft(0)(_ + _)
+//    val total = list.foldLeft(0)((a, b) => a.+(b))
+
+    val product = list.foldLeft(1)(_ * _)
+//    val product = list.foldLeft(1)((a, b) => a.*(b))
+
+    Seq("a", "b", "c").mkString(",")
+//    Seq("a", "b", "c").foldLeft("")((a, b) =>
+//    if(a.isEmpty) {
+//      a.+(b)
+//    } else {
+//      a.+(",").+(b)
+//    })
+
+    Map("a" -> 1, "b" -> 2, "c" -> 3).mapValues(_ + 1)
+
+//    Map("a" -> 1, "b" -> 2, "c" -> 3).map(new Function1[(String, Int), (String, Int)]{
+//      def apply(a: (String, Int)): (String, Int) = {
+//        (a._1, a._2.+(1))
+//      }
+//    })
   }
 
 }
